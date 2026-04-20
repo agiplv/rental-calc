51dfacbea4909e9269b84bc1bd4f303ed2b26e9e
agiplv <46893922+agiplv@users.noreply.github.com>
2026-04-20 18:27:48 +0000

    qqq

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: './',
   plugins: [react()],
-  server: { port: 5173 }
+  server: { port: 5173 },
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true
+  }
 })
